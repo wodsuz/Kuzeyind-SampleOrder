0c1e91ceca35f1c92424186058d8954837982a25
ongun demirag <ongun.demirag@gmail.com>
2022-02-17 17:42:14 +0300

    recommended items

--- components/RecommendedList.js
+++ components/RecommendedList.js
@@ -1,18 +1,22 @@
 import ProductCard from "./ProductCard";
 
-const ProdcutsList = ({ products }) => {
+const RecommenList = ({ products, currentProduct }) => {
   return (
     <div className="bg-gradient-to-r from-cyan-500 to-indigo-500">
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-extrabold text-gray-900 mb-6">Products</h2>
+        <h2 className="text-2xl font-extrabold text-gray-900 mb-6">
+          Recommended Products
+        </h2>
         <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-            <ProductCard key={product.node.id} product={product} />
-          ))}
+          {products.map((product) =>
+            product.node.id === currentProduct ? null : (
+              <ProductCard key={product.node.id} product={product} />
+            )
+          )}
         </div>
       </div>
     </div>
   );
 };
 
-export default ProdcutsList;
+export default RecommenList;
