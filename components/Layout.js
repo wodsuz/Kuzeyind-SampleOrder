949528fb0881577922afe9bcd53c94b65a426c21
ongun demirag <ongun.demirag@gmail.com>
2022-02-19 17:06:58 +0300

    seo-products page

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,10 @@
 import Footer from "./Footer";
 import Nav from "./Nav";
-
+import Headtitle from "./header";
 export default function Layout({ children }) {
   return (
     <div className="flex flex-col justify-between min-h-screen">
+      <Headtitle />
       <Nav />
       <main>{children}</main>
       <Footer />
